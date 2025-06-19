5a98fcae21e60fefeb4cb6702b386b75e5639556
TFPAdmin <github@thefamilyplan.xyz>
2025-06-18 22:18:11 -0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -2,6 +2,8 @@
 CREATE TABLE IF NOT EXISTS books (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
+    series VARCHAR(255) NOT NULL,
+    position VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     description TEXT,
     image_url VARCHAR(255),
